8dfed043d853ca3ed402073df68ad1fd177d9db0
eder <ing.eder.esc@gmail.com>
2026-06-15 18:12:35 -0600

    Actualizacion de textos de padres, padrinos

--- resources/views/livewire/boda.blade.php
+++ resources/views/livewire/boda.blade.php
@@ -79,9 +79,23 @@
                                     <i class="fas fa-heart ww-hero-divider-heart"></i>
                                     <span class="ww-hero-divider-line"></span>
                                 </div>
-                                <p class="ww-intro-text poppins_regular">Hoy agradezco a Dios por este momento tan especial.</p>
-                                <p class="ww-intro-text poppins_regular">Gracias a mis papás por su amor incondicional.</p>
-                                <p class="ww-intro-text poppins_regular ww-intro-text--highlight">Con ilusión celebro mis XV, feliz de compartir esta alegría con quienes siempre guardaré en mi corazón.</p>
+                                <p class="ww-intro-text poppins_regular">El tiempo pasa volando y, casi sin darme cuenta, hoy llego a mis 15 años agradeciendo a Dios por la vida que me dio; a mis padres, por su amor y apoyo incondicional, por nunca soltar mi mano; y a mi familia, por ser parte de mi camino.</p>
+                                <p class="ww-intro-text poppins_regular">Hoy dejo atrás una infancia llena de amor, bendiciones y recuerdos hermosos que nunca olvidaré.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center mt-4">
+                        <div class="col-lg-8 col-md-10 col-12" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                            <div class="ww-intro-card text-center">
+                                <p class="ww-intro-text poppins_regular">Como una mariposa que despliega sus alas, hoy comienzo a volar hacia un nuevo horizonte: nuevos sueños, nuevas metas y la misma alegría de siempre.</p>
+                                <p class="ww-intro-text poppins_regular">La vida es un libro de aventuras y hoy empiezo a escribir un nuevo capítulo en mi historia, con la bendición de Dios y de mis padres.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center mt-4">
+                        <div class="col-lg-8 col-md-10 col-12" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                            <div class="ww-intro-card text-center">
+                                <p class="ww-intro-text poppins_regular">¡Acompáñame a vivir este gran día: un sueño por cumplir, un momento para recordar y el inicio de mi juventud!</p>
                             </div>
                         </div>
                     </div>
@@ -108,14 +122,14 @@
                         </div>
                         <div class="row text-center pt-2">
                             <div class="col-md-4 mt-3" data-aos='fade-right'>
-                                <h3 class="h2 rouge-script">Armando Hernández </h3>
+                                <h3 class="h2 rouge-script">Uziel Yair Cerón Pérez</h3>
                                 <p class="pt-3 text-md-right text-sm-center poppins_regular ">Gracias a Dios por permitirme verte hoy convertida en una hermosa mujer. Ser tu padre es el mayor orgullo de mi vida. ¡Felices XV, hija! Te amo más allá del cielo.</p>
                             </div>
                             <div class="col-md-4 mb-4">
                                 <img class="img-fluid w-100" src="{{ asset('images/parents/parents_second.png') }}" style="max-width: 275px;" alt="mis_padres" data-aos="fade-down" data-aos-duration="1000"/>
                             </div>
                             <div class="col-md-4" data-aos='fade-right'>
-                                <h3 class="h2 rouge-script">Maria González</h3>
+                                <h3 class="h2 rouge-script">Lesly Cázares Gutiérrez</h3>
                                 <p class="pt-3 text-md-left text-sm-center poppins_regular">Gracias por enseñarme a ser mamá y compañera de vida. Hoy agradezco a Dios por tus 15 años y por permitirme caminar a tu lado. Te amo con todo mi corazón.</p>
                             </div>
                         </div>
@@ -139,7 +153,7 @@
                             <div class="col">
                                 <img class="img-fluid flower_class mb-2" src="{{ asset('images/parents/icon_parents.png') }}" alt="mis_padres" data-aos="flip-left" data-aos-duration="1000"/>
                                 <h1 class="h1 text-center  rouge-script www-title-rose-color"
-                                    data-aos="zoom-in-down" data-aos-duration="1000">Mis Padrinos De Velación
+                                    data-aos="zoom-in-down" data-aos-duration="1000">Padrinos de iglesia y velación
                                 </h1>
                                 <!-- <img class="img-fluid dress_class mb-2 pb-3"
                                      src="{{ asset('images/dress/green_dress-removebg.png') }}" alt="mis_padres" data-aos="flip-left" data-aos-duration="1000"/> -->
@@ -151,14 +165,14 @@
                         </div>
                         <div class="row text-center pt-2">
                             <div class="col-md-4 mt-3" data-aos='fade-right'>
-                                <h3 class="h2 rouge-script">José Cruz</h3>
+                                <h3 class="h2 rouge-script">José Cruz Guerrero Luna</h3>
                                 <p class="pt-3 text-md-right text-sm-center poppins_regular ">Mi querida Ashley, agradezco a Dios y a tus papás por permitirme ser tu padrino. Gracias por elegirme para acompañarte en este día tan especial. Que tu vida esté siempre llena de felicidad. Te quiero mucho.</p>
                             </div>
                             <div class="col-md-4 pb-4">
                                 <img class="img-fluid w-100 " src="{{ asset('images/dress/dress_principal_2.png') }}" style="max-width: 275px;" alt="mis_padres" data-aos="flip-left" data-aos-duration="1000"/>
                             </div>
                             <div class="col-md-4" data-aos='fade-right'>
-                                <h3 class="h2 rouge-script">Saraí Nolasco</h3>
+                                <h3 class="h2 rouge-script">Ivette Cázares Gutiérrez</h3>
                                 <p class="pt-3 text-md-left text-sm-center poppins_regular">Ashley, vuela alto y nunca dejes de creer en tus sueños. Hoy celebramos a la hermosa mujer en la que te estás convirtiendo. Siempre estaré para apoyarte.</p>
                             </div>
                         </div>
@@ -166,37 +180,6 @@
                 </div>
 
             <hr class="hr_second rounded">
-
-            {{--    --}}{{----}}{{-------------------     2       PADRINOS MISA --}}
-            <div class="ww-section" id="Padrinos">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <img class="img-fluid flower_class mb-2" src="{{ asset('images/parents/icon_parents.png') }}" alt="mis_padres" data-aos="flip-left" data-aos-duration="1000"/>
-                            <h1 class="h1 text-center rouge-script www-title-rose-color"
-                                data-aos="zoom-in-down" data-aos-duration="1000">Mis Padrinos De Iglesia
-                            </h1>
-                        </div>
-                    </div>
-                    <div class="row d-flex align-items-center justify-content-center pb-4">
-                        <div class="col-md-8 col-sm-10">
-                        </div>
-                    </div>
-                    <div class="row text-center pt-2">
-                        <div class="col-md-4 mt-3" data-aos='fade-right'>
-                            <h3 class="h2 rouge-script">Silviano Ortíz</h3>
-                            <p class="pt-3 text-md-right text-sm-center poppins_regular ">Mi querida Ashley, ser tu padrino es un honor; en tus XV te deseo una vida llena de amor y felicidad, siempre estaré para ti.  </p>
-                        </div>
-                        <div class="col-md-4 pb-4">
-                            <img class="img-fluid w-100" src="{{ asset('images/padrinos/example3.png') }}" style="max-width: 275px;" alt="mis_padres" data-aos="flip-left" data-aos-duration="1000"/>
-                        </div>
-                        <div class="col-md-4" data-aos='fade-right'>
-                            <h3 class="h2 rouge-script">Lidia Morales</h3>
-                            <p class="pt-3 text-md-left text-sm-center poppins_regular">Querida Ashley, vuela alto y persigue tus sueños; hoy despliegas tus alas y siempre estaré contigo para apoyarte. 💕.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
 {{--            --}}{{----}}{{----------            MIS PALABRAS --}}
             <div class="py-5">
@@ -207,8 +190,11 @@
                             <i class="fas fa-quote-left fa-3x quote_color"></i>
                             <div class="d-flex justify-content-center text-center">
                                 <div class="w-100 m-4 pb-4">
-                                    <h1 class="rouge-script white-letter" >
-                                        "Aquí y ahora, cierro una etapa y me abro a todo lo bueno que la vida tiene para mí"
+                                    <!-- <h1 class="rouge-script white-letter mb-4">
+                                        Sé que existen momentos inolvidables, pero compartirlos con las personas que más quiero los hace verdaderamente mágicos.
+                                    </h1> -->
+                                    <h1 class="rouge-script white-letter mb-0">
+                                        Aquí y ahora, cierro una etapa y me abro a todo lo bueno que la vida tiene para mí
                                     </h1>
                                 </div>
                             </div>
